dba2d338d0b704e54e8cb768f1532bfec09f35d0
harish414 <harishmopuri@gmail.com>
2021-06-27 11:05:34 +0530

    This is my commit

--- src/main/java/qaClickAcademy/App.java
+++ src/main/java/qaClickAcademy/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("JIRA1");
+        System.out.println("JIRA2");
+        System.out.println("JIRA3");
     }
 }
